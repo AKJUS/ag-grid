11be1901e398e5f5f8881875e95a6cd7838c2f9f
Salvatore Previti <salvatore.previti@ag-grid.com>
2025-09-02 10:46:27 +0100

    AG-15729 stable group maintain order (#11856)
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order
    
    * AG-15729-stable-group-maintain-order

--- testing/module-size/moduleDefinitions.ts
+++ testing/module-size/moduleDefinitions.ts
@@ -6,7 +6,7 @@ import type {
 
 export const AllGridCommunityModules: Record<`${CommunityModuleName}Module`, number> = {
     AlignedGridsModule: 6.88,
-    AllCommunityModule: 481.68,
+    AllCommunityModule: 482.0,
     CellApiModule: 0.28,
     CellStyleModule: 2.24,
     CheckboxEditorModule: 64.61,
