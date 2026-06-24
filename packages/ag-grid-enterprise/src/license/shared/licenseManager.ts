6dfdedc1a6bd5aa4713ca00f984ef8549ee276e3
AgGitDeployment <deployments.team@ag-grid.com>
2026-06-24 14:24:32 +0100

    Release 36.0.0 Prep

--- packages/ag-grid-enterprise/src/license/shared/licenseManager.ts
+++ packages/ag-grid-enterprise/src/license/shared/licenseManager.ts
@@ -17,7 +17,7 @@ export interface ILicenseManager {
 
 export class LicenseManager {
     // eslint-disable-next-line no-restricted-syntax
-    private static readonly RELEASE_INFORMATION: string = 'MTc4MjMwNjMyNTc5MQ==';
+    private static readonly RELEASE_INFORMATION: string = 'MTc4MjMwNzQ3MTgyMg==';
     // eslint-disable-next-line no-restricted-syntax
     private static licenseKey: string;
     // eslint-disable-next-line no-restricted-syntax
